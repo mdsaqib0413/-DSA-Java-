29fb1df7a9ed86716e0fc359cb26666040e63935
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-10 22:13:02 +0530

    Add files via upload

---
+++ BInary Search/LC_744_finf_smallest_letter_greater_than_target.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int st=0;
+        int end=letters.length-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(letters[mid]>target){
+                end=mid-1;
+            }else{
+                st=mid+1;
+            }
+        }
+        return letters[st%letters.length];
+    }
+}
