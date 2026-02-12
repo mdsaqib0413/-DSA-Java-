117f9d75a8f5fbde5ae7b44dad1f56726e4c2cc4
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-12 21:39:14 +0530

    Add files via upload

---
+++ Sorting/LC_1346_check_if_N_and_Its_double_exists.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[j]==arr[i]*2 || arr[i]==arr[j]*2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
