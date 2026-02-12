48809c0592f1e696e4046e1de73d508cefa17369
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-12 22:05:54 +0530

    Add files via upload

---
+++ Sorting/LC_2154_Keep_Multiplying_Found_Values_by_Two.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        for(int i=0; i<nums.length; i++){
+            if(original==nums[i]) return findFinalValue(nums, original*2);
+        }
+        return original;
+    }
+}
