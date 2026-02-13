13c2546ce8103262f9133519711c8692fc63a7d8
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-13 23:16:23 +0530

    Add files via upload

---
+++ Arrays/LC_2441_Largest_Positive_Integer_That_Exists_With_Its_Negative.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+        for(int j=nums.length-1; j>=0; j--){
+            for(int i=0; i<nums.length; i++){
+            if(nums[j]==-nums[i]){
+                return nums[j];
+            }
+        }}
+        return -1;
+    }
+}
