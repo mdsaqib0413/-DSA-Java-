324df2827168a400a8f9e7b200f9c814d293295a
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-17 22:56:12 +0530

    Add files via upload

---
+++ Math/LC_231_Power_of_Two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>1){
+            if(n%2==0){
+                n=n/2;
+            }else{
+                return false;
+            }
+        }
+        if(n==1) return true;
+        return false;
+    }
+}
