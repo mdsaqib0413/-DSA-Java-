529b29692d732305e0724400e75feb16e7fc53ef
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-19 23:13:53 +0530

    Add files via upload

---
+++ Math/LC_509_Fibonacci_Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int fib(int n) {
+        int a=0, b=1;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        for(int i=2; i<=n; i++){
+            int temp=b;
+            b=b+a;
+            a=temp;
+        }
+        return b;
+    }
+}
