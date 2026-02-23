fbc304d1e7e919f3a729082f3c585bed4ec30eb6
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-02-23 16:02:51 +0530

    Add files via upload

---
+++ Arrays/LC_136_Single_Number.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums.length==1) return nums[0];
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if(set.contains(nums[i])){
+                set.remove(nums[i]);
+            }else{
+                set.add(nums[i]);
+            }
+        }
+        return set.iterator().next();
+    }
+}
