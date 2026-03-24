b719a89eea41e0d994c6b679c90fa2027b39a9c6
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-03-24 13:28:57 +0530

    Add files via upload

---
+++ Stack/LC_155_Min_Stack.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+class MinStack {
+    Stack<Integer> st=new Stack<>();
+    Stack<Integer> min=new Stack<>();
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        st.push(val);
+        if(min.isEmpty() || val<=min.peek()){
+            min.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(st.peek().equals(min.peek())){
+            min.pop();
+        }
+        st.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();   
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.peek();
+ * int param_4 = obj.getMin();
+ */
