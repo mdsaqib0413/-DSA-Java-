ee813cda61c7c595ef7664af19096ce9c0f12896
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-03-28 19:42:56 +0530

    Add files via upload

---
+++ Arrays/LC_1431_Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> res=new ArrayList<>();
+        int max=0;
+        int n=candies.length;
+        for(int i=0; i<n; i++){
+            if(candies[i]>max) max=candies[i];
+        }
+        for(int i=0; i<n; i++){
+            if(candies[i]+extraCandies>=max) res.add(true);
+            else res.add(false);
+        }
+        return res;
+    }
+}
