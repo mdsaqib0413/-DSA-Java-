ed14b15dca9a0b04aed4f1c9aceb07c20c3091d3
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-03-28 19:17:38 +0530

    Add files via upload

---
+++ Arrays/LC_724_Find_Pivot_Index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
+        }
+        int suml=0;
+        int sumr=0;
+        for(int i=0; i<n; i++){
+            sumr=sum-suml-nums[i];
+            if(sumr==suml) return i;
+            suml+=nums[i];
+        }
+        return -1;
+    }
+}
