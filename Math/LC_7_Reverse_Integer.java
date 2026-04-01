65fe9daea39ecfae9f7c9aebce76231e9aabc551
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-04-01 20:33:52 +0530

    Add files via upload

---
+++ Math/LC_7_Reverse_Integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        int res=0;
+        while(x!=0){
+            int dig=x%10;
+            x/=10;
+            if(res>Integer.MAX_VALUE/10 || res<Integer.MIN_VALUE/10) return 0;
+            res=res*10+dig;
+        }
+        return res;
+    }
+}
