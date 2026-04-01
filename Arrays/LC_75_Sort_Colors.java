626932f3507e8a343d23fe57f36e1b19bf95862d
mdsaqib0413 <mdsaqib0413@gmail.com>
2026-04-01 20:18:27 +0530

    Add files via upload

---
+++ Arrays/LC_75_Sort_Colors.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l=0, m=0, h=nums.length-1;
+        while(m<=h){
+            if(nums[m]==0){
+                int temp=nums[m];
+                nums[m]=nums[l];
+                nums[l]=temp;
+                l++;
+                m++;
+            }else if(nums[m]==1){
+                m++;
+            }else{
+                int temp=nums[m];
+                nums[m]=nums[h];
+                nums[h]=temp;
+                h--;
+            }
+        }
+    }
+}
