8ed8af7df6d74981ca7cba698196437f83bf03a2
Shaik Mohammed Younus Saqib <mdsaqib0413@gmail.com>
2026-08-04 23:38:52 +0530

    Update LC_342_Power_of_Four.java

--- Math/LC_342_Power_of_Four.java
+++ Math/LC_342_Power_of_Four.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==1 || n==4) return true;
-        if(n<=0 || n%4!=0) return false;
-        boolean x=isPowerOfFour(n/4);
-        return x;
+        if(n<=0) return false;
+        while(n%4==0) n/=4;
+        return n==1;
     }
 }
